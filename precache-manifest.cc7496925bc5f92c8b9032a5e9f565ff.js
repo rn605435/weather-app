eb222f72c0ef5499d786ad63410cb4870edbc1ae
Nicolas <nicolas.rochette@etu.unice.fr>
2018-12-03 18:24:52 +0100

    Updates

---
+++ precache-manifest.cc7496925bc5f92c8b9032a5e9f565ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rn605435/weather-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "161a5f81d837924887b0",
+    "url": "/rn605435/weather-app/static/js/runtime~main.161a5f81.js"
+  },
+  {
+    "revision": "585686f3d52b41834110",
+    "url": "/rn605435/weather-app/static/js/main.585686f3.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/rn605435/weather-app/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "585686f3d52b41834110",
+    "url": "/rn605435/weather-app/static/css/main.5847bdc9.chunk.css"
+  },
+  {
+    "revision": "e446ef909c32e5d199c3805468326be8",
+    "url": "/rn605435/weather-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/rn605435/weather-app/precache-manifest.cc7496925bc5f92c8b9032a5e9f565ff.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/rn605435/weather-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
